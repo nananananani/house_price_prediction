d6ea286d5c76447e4e1bbd56e79805835b712071
nananananani <ayuuki478@gmail.com>
2025-09-13 18:57:45 +0530

    loading dataset

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -4,3 +4,17 @@ import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 #using california housing dataset from sklearn
+from sklearn.datasets import fetch_california_housing
+#implement train and test split
+from sklearn.model_selection import train_test_split
+#data preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+#loading dataset
+housing_data = fetch_california_housing(as_frame=True)   # load dataset as dataframe
+df = housing_data.frame
+print("First 5 rows of dataset:")
+print(data.head())
